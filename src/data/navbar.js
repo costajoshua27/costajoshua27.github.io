84939baa0774a1422f8c5d426603e3a052671046
Joshua Costa <costa.joshua27@gmail.com>
2020-08-26 22:06:19 -0700

    Updates

--- src/data/navbar.js
+++
@@ -1,6 +0,0 @@
-import resume from '../images/current_resume.pdf';
-const emailLink = 'mailto:costajoshua27@gmail.com';
-const linkedinLink = 'https://www.linkedin.com/in/joshua-costa-b2010416b/';
-const githubLink = 'https://github.com/costajoshua27';
-
-export { resume, emailLink, linkedinLink, githubLink };
