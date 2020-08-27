9f36d502936cb3600232d68dbfc40ac9bc360bc2
Joshua Costa <costa.joshua27@gmail.com>
2020-08-26 21:59:59 -0700

    Updates

---
+++ precache-manifest.c807453d490f0333fbe58548ec455bbb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9c8b20f21aa9ba0f96021f3fd4c98f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e17d9a4baebcc9ae7a8b",
+    "url": "/static/css/main.d38669f4.chunk.css"
+  },
+  {
+    "revision": "dfdc66c29dac21cfa23b",
+    "url": "/static/js/2.faaa1417.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.faaa1417.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e17d9a4baebcc9ae7a8b",
+    "url": "/static/js/main.5fce079c.chunk.js"
+  },
+  {
+    "revision": "833afa376532221ce86f",
+    "url": "/static/js/runtime-main.a2e0dc0d.js"
+  },
+  {
+    "revision": "01ceac91da6015eef6d55d63cb959ff3",
+    "url": "/static/media/GradeCalculator.01ceac91.png"
+  },
+  {
+    "revision": "12ea566c249e8e5383a3e81de0dbdfdd",
+    "url": "/static/media/current_resume.12ea566c.pdf"
+  },
+  {
+    "revision": "637ba8feb005d576496800143b2f848e",
+    "url": "/static/media/headshot.637ba8fe.jpeg"
+  },
+  {
+    "revision": "a54a3c3dae2c1a9bfea0091c79f0d0d1",
+    "url": "/static/media/jTrivia.a54a3c3d.png"
+  },
+  {
+    "revision": "3693fcea2b38e038f325b9bbeb4e2217",
+    "url": "/static/media/reFresh.3693fcea.png"
+  }
+]);
